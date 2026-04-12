db2cb55eee798ec0104c4d0f6f271e3ba7e4c14d
parmin-ctrl <parminabedi2022@gmail.com>
2026-04-11 17:36:24 -0700

    added some code, modified map, attached score

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,8 +1,44 @@
 extends CharacterBody2D
 
-const SPEED = 500
-const JUMP_VELOCITY = -700
-const GRAVITY = 600
+const SPEED = 900
+const JUMP_VELOCITY = -1100
+const GRAVITY = 800
+
+const scorescene = preload("res://scenes/tracker.tscn")
+		# load our little score label node - "const" because we don't need to change this value
+
+	# also preload the images you want to use as icons here:
+const icon1 = preload("res://assets/burrito_sprite.png")	# just usin the default godot icon here :sob:
+					
+var scores = [
+		["coins", "5", icon1, Vector2(0, 100)],
+		["bushes", "0", icon1, Vector2(0, 150)],
+		["wood", "20", icon1, Vector2(0, 200)]
+	]
+		# ok so this is where we put all the scores we want to track, along with the score we want them
+		# to start at, the image we want them to have, and the coordinates we want to put them at.
+		# make sure you give each label a different position!! unless you really want them to overlap?
+		# you can have them along the top, under the player (like i have them), one in each corner,
+		# or even randomly scattered if you want that to make your game extra unique!!
+
+var score_labels = []
+		# this is where we'll be putting all of our instantiated score nodes,
+		# just to save them for when we need to refer to them
+		# (for example to check if we have enough of a certain item)
+
+func _ready() -> void:
+		for score in scores:
+			var score_label = scorescene.instantiate()
+					
+			score_label.whatitscounting = score[0]
+			score_label.val = int(score[1]) # the starting value of the item
+			score_label.text = score[1] # this is the actual text displayed
+			score_label.get_node("TextureRect").texture = score[2]
+			score_label.position = score[3]
+						
+			# now, let's add it as a child and chuck it in the score_labels storage list:
+			add_child(score_label)
+			score_labels.append(score_label)
 
 func _physics_process(delta):
 	# Apply gravity
---
+++ scenes/tracker.gd
@@ -0,0 +1,4 @@
+extends Label
+
+var whatitscounting : String # this is called "typing", it sets the datatype that the variable will use
+var val : int
